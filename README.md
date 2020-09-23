c181cb549d9c8d0c0589089b583d9516e22a21ed
Gustavo Luszczynski <luszczynski@gmail.com>
2020-09-23 17:36:24 -0300

    get and post method created

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Purpose
 
-This is a simple project to help anyone to debug requests. When using a proxy or any kind of API Management solution, it may help when you can not call external APIs (https://echo-api.3scale.net:443 or http://postman-echo.com/get for example) and want to check how your payload is reaching the backend.
+This is a simple project to help anyone to debug http requests. When using a proxy or any kind of API Management solution, it may help you when you can not call external APIs (https://echo-api.3scale.net:443 or http://postman-echo.com/get for example) and want to check how your payload is reaching the backend.
 
 ## How to install
 
@@ -14,9 +14,8 @@ oc expose svc/debug --path=/debug -n debug
 
 ## Testing
 
-For example:
-
 ```bash
+# Using httpie
 http http://debug-debug.apps.brasilia-69e5.open.redhat.com/debug
 
 HTTP/1.1 200 OK
@@ -39,4 +38,7 @@ Set-Cookie: 0583d7385ac1fa0c0483f966682199e4=be6881f09275d76e9ef371c961254aab; p
     "remoteAddress": "10.1.6.1:41328",
     "uri": "/debug"
 }
+
+# Using curl
+curl http://debug-debug.apps.brasilia-69e5.open.redhat.com/debug
 ```
--- src/main/java/com/redhat/DebugResource.java
+++ src/main/java/com/redhat/DebugResource.java
@@ -3,8 +3,10 @@ package com.redhat;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
@@ -31,7 +33,31 @@ public class DebugResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Debug hello(String body) {
+    public Debug helloGet(@QueryParam("delay") Long delay) throws InterruptedException {
+        if(delay != null)
+            Thread.sleep(delay);
+
+        debug.path = info.getPath();
+        debug.remoteAddress = request.remoteAddress().toString();
+        debug.headers = request.headers();
+        debug.pathParams = info.getPathParameters();
+        debug.queryParams = info.getQueryParameters();
+        debug.cookieMap = request.cookieMap();
+        debug.method = request.method();
+        debug.uri = request.uri();
+        debug.count = debugService.increment();
+
+        LOG.info(debug.toString());
+
+        return debug;
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Debug helloPost(@QueryParam("delay") Long delay, String body) throws InterruptedException {
+        if(delay != null)
+            Thread.sleep(delay);
+
         debug.path = info.getPath();
         debug.remoteAddress = request.remoteAddress().toString();
         debug.headers = request.headers();
